75ee255ab0f2c3584454abc7b4999d1642f68226
Giulia-M <giuliamaggio16@gmail.com>
2021-11-15 15:35:22 +0100

    handleError in login

--- app01/src/app/auth/auth.service.ts
+++ app01/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
@@ -27,29 +27,38 @@ export class AuthService {
           returnSecureToken: true,
         }
       )
-      .pipe(
-        catchError((errorResp) => {
-          let errorMessage = ' errore sconosciuto ';
-          if (!errorResp.error || !errorResp.error.error) {
-            return throwError(errorMessage);
-          }
-          switch (errorResp.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'Questa email è già registrata';
-          }
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7yqauVl8KAexl98vbINJBvcC6FPQWPFk',
-      {
-        email: email,
-        password: password,
-        returnSecureToken: true,
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyB7yqauVl8KAexl98vbINJBvcC6FPQWPFk',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(errorResp: HttpErrorResponse) {
+    let errorMessage = ' errore sconosciuto ';
+    if (!errorResp.error || !errorResp.error.error) {
+      return throwError(errorMessage);
+    }
+    switch (errorResp.error.error.message) {
+      case 'EMAIL_EXISTS':
+        errorMessage = 'Questa email è già registrata';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = ' Questa email non esiste';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = ' La password non è corretta';
+        break;
+    }
+    return throwError(errorMessage);
   }
 }
